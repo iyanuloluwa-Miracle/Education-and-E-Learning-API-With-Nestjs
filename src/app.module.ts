6b4d730b2fd44c94a464ab66f1ad989426376e51
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-10-16 14:35:26 +0100

    :sparkles: feat: folder creation for auth and course

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, CourseModule],
   controllers: [AppController],
   providers: [AppService],
 })
