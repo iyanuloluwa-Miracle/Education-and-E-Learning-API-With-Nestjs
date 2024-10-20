9aacbbc797831413923ba3ebe514cda823eb6d50
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-10-20 16:35:58 +0100

    :sparkles: feat:authentication endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { CourseModule } from './course/course.module';
 import { UserModule } from './user/user.module';
 import { ProgressModule } from './progress/progress.module';
-import { ToolsModule } from './tools/tools.module';
+import { ToolsModule } from './tools/quiz.module';
 
 @Module({
   imports: [AuthModule, CourseModule, UserModule, ProgressModule, ToolsModule],
---
+++ src/auth/dtos/auth.dto.ts
@@ -0,0 +1,11 @@
+export class AuthDto {
+  email: string;
+  password: string;
+}
+
+export class RegisterDto {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
