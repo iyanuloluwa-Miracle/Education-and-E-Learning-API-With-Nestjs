d860c101db3d9f046f7d7787b4b54e3563960ca8
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-10-31 13:14:12 +0100

    :sparkles: feat: Project restructuring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ExpensesModule } from './expenses/expenses.module';
 
 
 @Module({
   controllers: [AppController],
-  providers: [AppService], 
+  providers: [AppService],
+  imports: [AuthModule, UsersModule, ExpensesModule], 
 })
 export class AppModule {}
