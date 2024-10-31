30a15f07a80c364d3a6a30f6adbb676b77064ac5
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-10-31 12:17:59 +0100

    :sparkles: fixes: latest fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { CourseModule } from './course/course.module';
-import { UserModule } from './user/user.module';
-import { ProgressModule } from './progress/progress.module';
-import { ToolsModule } from './tools/quiz.module';
+
 
 @Module({
-  imports: [AuthModule, CourseModule, UserModule, ProgressModule, ToolsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService], 
 })
-export class AppModule {}
+export class AppModule {}
